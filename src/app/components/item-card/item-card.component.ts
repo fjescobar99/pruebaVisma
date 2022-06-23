0a60ec9b4c0c256c5b527f37e55cbed8f36d146d
fjescobar99 <45143998+fjescobar99@users.noreply.github.com>
2022-06-23 00:44:15 -0300

    Created repository

---
+++ src/app/components/item-card/item-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-card',
+  templateUrl: './item-card.component.html'
+})
+export class ItemCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
